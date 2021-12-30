e5b4d9c98173d6da14ca26b7c7021c462e3885a9
donkeyson <88279704+donkeyson@users.noreply.github.com>
2021-12-30 12:29:25 +0900

    Create sol_variables.sol

---
+++ solidity_day2/sol_variables.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.6.0;
+
+contract SimpleStorage {
+
+    uint256 favoriteNumber = 5;
+    bool favoriteBool = true;
+    string favoriteString = 'String';
+    int256 favoriteInt = 5;
+    address favoriteAddress = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+    bytes32 favoriteBytes = 'cat';
+    
+}
