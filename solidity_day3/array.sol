bcb12713821e3bdbcb076d10757d4f5bd987c908
donkeyson <88279704+donkeyson@users.noreply.github.com>
2021-12-31 15:13:32 +0900

    Create array.sol

---
+++ solidity_day3/array.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.6.0;
+
+contract SimpleStorage {
+
+    uint256 favoriteNumber;
+    bool favoriteBool;
+
+    struct People {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    People[] public people;
+
+    
+    function store(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+    }
+
+    function retrieve(uint256 favoriteNumber) public pure {
+        favoriteNumber + favoriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        people.push(People(favoriteNumber: _favoriteNumber, name: _name}));
+    }
+}
